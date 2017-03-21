0d4e771e126b564c5c7b86eb1b535c2afec1e5ce
Kyle Tallman <kwtallman@umass.edu>
2017-03-21 16:39:57 -0400

    react scaffolding

--- app/app.js
+++ app/app.js
@@ -3,19 +3,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Each major browser view user interface must be imported.
-import UI01 from './components/ui-01.js';
-import UI02 from './components/ui-02.js';
+import UI01 from './build/js/ui-01.js';
+import UI02 from './build/js/ui-02.js';
+import UI03 from './build/js/ui-03.js';
+import UI04 from './build/js/ui-04.js';
+import UI05 from './build/js/ui-05.js';
+import UI06 from './build/js/ui-06.js';
 
 // For each view conditionally determine which view to display
 // depending on if the ID is present in the HTML.
-if (document.getElementById('ui-01') !== null) {
+if (document.getElementById('addassignment') !== null) {
   ReactDOM.render(
     <UI01 />,
-    document.getElementById('ui-01')
+    document.getElementById('addassignment')
   );
-} else if (document.getElementById('ui-02') !== null) {
+} else if (document.getElementById('calendar') !== null) {
   ReactDOM.render(
     <UI02 />,
-    document.getElementById('ui-02')
+    document.getElementById('calendar')
+  );
+} else if (document.getElementById('dragupload') !== null) {
+  ReactDOM.render(
+    <UI03 />,
+    document.getElementById('dragupload')
+  );
+} else if (document.getElementById('colespage') !== null) {
+  ReactDOM.render(
+    <UI04 />,
+    document.getElementById('colespage')
+  );
+} else if (document.getElementById('home') !== null) {
+  ReactDOM.render(
+    <UI05 />,
+    document.getElementById('home')
+  );
+} else if (document.getElementById('manual') !== null) {
+  ReactDOM.render(
+    <UI06 />,
+    document.getElementById('manual')
+  );
+} else if (document.getElementById('profile') !== null) {
+  ReactDOM.render(
+    <UI07 />,
+    document.getElementById('profile')
   );
 }
