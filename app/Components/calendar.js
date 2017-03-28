485e71407fe51b9c17043be784bf92fd3e614e9c
Eric Atwood <e.rock.at@gmail.com>
2017-03-28 15:35:54 -0400

    replaced calendar for now

--- app/Components/calendar.js
+++ app/Components/calendar.js
@@ -4,57 +4,7 @@ export default class Calendar extends React.Component {
   render() {
     return (
       <div>
-      	<div className="month">
-		      <ul>
-		        <li className="prev">&#10094;</li>
-		        <li className="next">&#10095;</li>
-		        <li>
-		          February<br />
-		          <span style="font-size:18px">2017</span>
-		        </li>
-		      </ul>
-		    </div>
-
-		    <ul className="weekdays">
-		      <li>Mo</li>
-		      <li>Tu</li>
-		      <li>We</li>
-		      <li>Th</li>
-		      <li>Fr</li>
-		      <li>Sa</li>
-		      <li>Su</li>
-		    </ul>
-
-		    <ul className="days">
-		      <li>1</li>
-		      <li>2</li>
-		      <li>3</li>
-		      <li>4</li>
-		      <li>5</li>
-		      <li>6</li>
-		      <li>7</li>
-		      <li>8</li>
-		      <li>9</li>
-		      <li>10</li>
-		      <li>11</li>
-		      <li>12</li>
-		      <li>13</li>
-		      <li>14</li>
-		      <li>15</li>
-		      <li>16</li>
-		      <li>17</li>
-		      <li>18</li>
-		      <li>19</li>
-		      <li><span className="active">20</span></li>
-		      <li>21</li>
-		      <li>22</li>
-		      <li>23</li>
-		      <li><span className="due">24</span></li>
-		      <li>25</li>
-		      <li>26</li>
-		      <li><span className="due">27</span></li>
-		      <li>28</li>
-		    </ul>
+      	
 		</div>
 		)
 	}
