f4edb05488aed3672ac490e86d6fcf9aafeffc90
Cole Arvidson <rarvidson@umass.edu>
2017-04-06 17:12:55 -0400

    finished database sorta

--- app/database.js
+++ app/database.js
@@ -7,30 +7,124 @@ var initialData = {
       "_id": 1,
       "userName": "Cole",
       "accounts": 1,
-      "assignmnets": 1
+      "assignList": 1
     },
     "2": {
       "_id": 2,
       "userName": "Elliot",
       "accounts": 2,
-      "assignmnets": 2
-    },
+      "assignList": 2
+    }
+  },
 
   "accounts": {
     "1": {
-      "type": "Moodle, Blackboard",
+      "type": [
+      "Moodle",
+      "Blackboard"
+      ],
       "userEmail": "cole.arvid@something.edu",
       "password": "cmpsci326"
     },
     "2": {
-      "type": "OWL",
+      "type": [
+        "OWL"
+      ],
       "userEmail": "elliot.crunch@something.edu",
       "password": "crunchy"
-    },
+    }
+  },
 
-  "assignments": {
+  "assignList": {
     "1": {
-      
+      "assignmnet": [
+        {
+        "_id": 1,
+        "dueDate": 1492041540,
+        "course": "CmpSci326",
+        "aLink": "https://moodle.umass.edu/"
+        },
+        {
+        "_id": 2,
+        "dueDate": 1491523140,
+        "course": "English101",
+        "aLink": "https://uma.umassonline.net/"
+        }
+      ]
+    },
+    "2": {
+      "assignmnet": [
+        {
+        "_id": 1,
+        "dueDate": 1492041540,
+        "course": "CmpSci326",
+        "aLink": "https://moodle.umass.edu/"
+        },
+        {
+        "_id": 2,
+        "dueDate": 1491523140,
+        "course": "English101",
+        "aLink": "https://uma.umassonline.net/"
+        }
+      ]
     }
   }
+};
+
+var data = JSON.parse(localStorage.getItem('grumber_data'));
+if (data === null) {
+  data = JSONClone(initialData);
+}
+
+function JSONClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+export function readDocument(collection, id) {
+  return JSONClone(data[collection][id]);
+}
+
+export function writeDocument(collection, changedDocument) {
+  var id = changedDocument._id;
+  data[collection][id] = JSONClone(changedDocument);
+  localStorage.setItem('grumber_data', JSON.stringify(data));
+}
+
+export function addDocument(collectionName, newDoc) {
+  var collection = data[collectionName];
+  var nextId = Object.keys(collection).length;
+  while (collection[nextId]) {
+    nextId++;
   }
+  newDoc._id = nextId;
+  writeDocument(collectionName, newDoc);
+  return newDoc;
+}
+
+/**
+ * Reset our browser-local database.
+ */
+export function resetDatabase() {
+  localStorage.setItem('grumber_data', JSON.stringify(initialData));
+  data = JSONClone(initialData);
+}
+
+/**
+ * Reset database button.
+ */
+class ResetDatabase extends React.Component {
+  render() {
+    return (
+      <button className="btn btn-default" type="button" onClick={() => {
+        resetDatabase();
+        window.alert("Database reset! Refreshing the page now...");
+        document.location.reload(false);
+      }}>Reset Mock DB</button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ResetDatabase />,
+  document.getElementById('fb-db-reset')
+);
