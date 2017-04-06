62c80a8258c77af0b08489b00f11a8970df10459
Cole Arvidson <rarvidson@umass.edu>
2017-04-06 15:58:09 -0400

    database file and beginnings

---
+++ app/database.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var initialData = {
+  "users": {
+    "1": {
+      "_id": 1,
+      "userName": "Cole",
+      "accounts": 1,
+      "assignmnets": 1
+    },
+    "2": {
+      "_id": 2,
+      "userName": "Elliot",
+      "accounts": 2,
+      "assignmnets": 2
+    },
+    "3": {
+      "_id": 3,
+      "userName": "Eric",
+      "accounts": 3,
+      "assignmnets": 3
+    },
+    "4": {
+      "_id": 4,
+      "userName": "Kyle",
+      "accounts": 4,
+      "assignmnets": 4
+    },
+    "5": {
+      "_id": 5,
+      "userName": "Zach",
+      "accounts": 5,
+      "assignmnets": 5
+    }
+  },
+  "accounts": {
+
+  }
