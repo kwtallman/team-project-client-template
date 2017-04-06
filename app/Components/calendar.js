7268097020747ef92be708aaf1ab2e93640b19ed
Eric Atwood <e.rock.at@gmail.com>
2017-04-06 17:18:45 -0400

    working on implementing BigCalendar

--- app/Components/calendar.js
+++ app/Components/calendar.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import Colebar from './colebar.js'
+
+BigCalendar.setLocalizer(
+  BigCalendar.momentLocalizer(moment)
+);
 
 export default class Calendar extends React.Component {
+	
   render() {
     return (
       <div>
-      	HELLO WORLD
+      <Colebar />
+      	Hello world
+
+      	<BigCalendar />
+      	
 		</div>
 		)
 	}
