4f5e3ecd33dbc0c96f7d2065584be27e9236760c
Kyle Tallman <kyletallman1@gmail.com>
2017-04-08 23:39:23 -0400

    updated db & server, updating manual

--- app/Components/manual.js
+++ app/Components/manual.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import LeftSide from './leftside.js';
+import Colebar from './colebar.js';
 
 export default class Manual extends React.Component {
   render() {
     return (
       <div>
         <LeftSide />
+        <Colebar />
       <div className="container">
         <p className="headtext">Manual Deadline Entry</p>
         <div className="row">
           <div className="col-md-4"></div>
           <div className="col-md-4">
-            <form>
+            <form method="post">
               <div className="form-group row">
                 <label htmlFor="classinput" className="col-2 col-form-label">Class</label>
                 <div className="col-10">
