cdcb20385218dfb66a762f36914fb9d42a4b1ba6
Kyle Tallman <kyletallman1@gmail.com>
2017-04-10 05:01:55 -0400

    tried to get router to work.. it doesn't

--- app/app.js
+++ app/app.js
@@ -2,9 +2,9 @@
 //import React from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { IndexRoute, Router, Route, hashHistory } from 'react-router'
 
 import localizer from 'react-big-calendar/lib/localizers/globalize';
-
 import 'react-big-calendar/lib/less/styles.less';
 // Each major browser view user interface must be imported.
 
@@ -12,9 +12,32 @@ import Manual from './Components/manual.js';
 import Home from './Components/home.js';
 import AddAssignment from './Components/addAssignment.js';
 import Profile from './Components/profile.js';
-import Dragupload from './Components/dragupload.js';
 import Colespage from './Components/colespage.js';
+import routerPage from './Components/routerpage.js';
 
+class App extends React.Component {
+  render() {
+    return (
+      <Router history={hashHistory}>
+        <Route path="/" component={routerPage}>
+          <IndexRoute component = {Home} />
+          <Route path = "manual" component = {Manual} />
+          <Route path = "addAssignment" component = {AddAssignment} />
+          <Route path = "profile" component = {Profile} />
+          <Route path = "colespage" component = {Colespage} />
+        </Route>
+      </Router>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+
+/*
 // For each view conditionally determine which view to display
 // depending on if the ID is present in the HTML.
 if (document.getElementById('manual') !== null) {
@@ -48,3 +71,4 @@ if (document.getElementById('manual') !== null) {
     document.getElementById('dragupload')
   );
 }
+*/
