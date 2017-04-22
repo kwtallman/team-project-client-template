a6ebc137b7beabaabdf5d63d835f1b1e5105ee97
Elliot DeBruyn <Elliot DeBruyn>
2017-04-21 21:45:01 -0400

    Adding more framework for the database

--- app/server.js
+++ app/server.js
@@ -37,3 +37,35 @@ export function getProfileData(user, cb) {
   profData.contents = profData.contents.map(getProfileSync);
   emulateServerReturn(profData, cb);
 }
+
+function getAssign(assignId) {
+  var assignList = readDocument('assignList', assignListId);
+  assignList.assignment = assignList.assignment.map((id) => readDocument('users', id));
+  assignList.assignment.forEach((assignData) => {
+    assignData.assignId = readDocument('users', assignList.assignmnet.assignId);
+  });
+  return assignList;
+}
+
+//FOR MONGO (LATER)
+// db.collection('assignments').findOne({
+//   _id: assignID,
+//   "contents.author": fromUser
+// }, function(err, assignment) {
+//   if (err) {
+//     return sendDatabaseError(res, err);
+//   } else if (assignment === null) {
+//     return res.status(400).end();
+//   }
+
+//PUT methods for organization
+app.put('/AddAssignment/:getAssign/content', function(req, res) {
+  var fromUser = new ObjectID(getUserIdFromToken(req.get('Authorization')));
+  var assignId = new ObjectID(req.params.assignId);
+}
+
+app.put('/Profile/:getProfileData/content', function(req, res) {
+    var fromUser = new ObjectID(getUserIdFromToken(req.get('Authorization')));
+    //var for each component (name, username, email, password)
+
+}
