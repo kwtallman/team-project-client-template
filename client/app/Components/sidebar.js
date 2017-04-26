0866d37f44e7ff421ea2c101a325ad760d74fcd2
Zach Tandara-Kuhns <ztandarakuhn@umass.edu>
2017-04-25 20:25:43 -0400

    Fixed sidebar buttons to actually bring you places (and reset DB)

--- client/app/Components/sidebar.js
+++ client/app/Components/sidebar.js
@@ -10,15 +10,17 @@ export default class LeftSide extends React.Component {
         <ul className="nav nav-pills nav-stacked sidebar">
           <br />
           <li role="presentation">Profile</li>
-          <li role="presentation"><a href="#"><span className="glyphicon glyphicon-user"></span> Edit</a></li>
+          <li role="presentation"><a href="profile.html"><span className="glyphicon glyphicon-user"></span> Edit</a></li>
           <li role="presentation">Assignments</li>
-          <li role="presentation"><a href="#"><span className="glyphicon glyphicon-calendar"></span> Calendar</a></li>
-          <li role="presentation" className="active"><a href="#"><span className="glyphicon glyphicon-list-alt"></span> List</a></li>
+          <li role="presentation"><a href="home.html"><span className="glyphicon glyphicon-calendar"></span> Calendar</a></li>
           <li role="presentation"><a href="#"><span className="glyphicon glyphicon-plus"></span> New</a></li>
           <li role="presentation">Entry</li>
-          <li role="presentation"><a href="#"><span className="glyphicon glyphicon-plus"></span> Manual</a></li>
-          <li role="presentation"><a href="#"><span className="glyphicon glyphicon-signal"></span> Automatic</a></li>
+          <li role="presentation"><a href="manual.html"><span className="glyphicon glyphicon-plus"></span> Manual</a></li>
+          <li role="presentation"><a href="AddAssignment.html"><span className="glyphicon glyphicon-signal"></span> Automatic</a></li>
           <li role="presentation">DEBUG</li>
+          <li role="presentation">{/*<a href="#"><span className="glyphicon glyphicon-repeat"></span> Reset Database</a>*/}
+            <ResetDatabase />
+          </li>
 
         </ul>
       </div>
